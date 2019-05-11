31bbd45193bba7a9b422db1236efccf93cafc011
Maheshwar Muttal <maheshmuttal@gmail.com>
2019-05-11 22:08:09 +0530

    facotry pattern update

--- FactoryDesignPattern/src/main/java/CurrencyFactory.java
+++ FactoryDesignPattern/src/main/java/CurrencyFactory.java
@@ -6,23 +6,23 @@ one of the core design pattern which is used heavily not only in JDK but also in
 
 Good example ":
 1) JDBC , application no need to know what database it wil be used.
-2) In java, enables the subclasses to provide extended version of an object (Ex: Currency Factory)
+2) In java, enables the subclasses to provide extended version of an object (Ex: Currency FactoryMain)
 
-When to use Factory design pattern in Java
+When to use FactoryMain design pattern in Java
 
-1) Static Factory methods are common in frameworks where library code needs to create objects of types which may be sub classed by applications using the framework.
+1) Static FactoryMain methods are common in frameworks where library code needs to create objects of types which may be sub classed by applications using the framework.
 2) Some or all concrete products can be created in multiple ways, or we want to leave open the option that in the future there may be new ways to create the concrete product.
-3) Factory method is used when Products don't need to know how they are created.
+3) FactoryMain method is used when Products don't need to know how they are created.
 4) We  can use factory pattern where we have to create an object of any one of sub-classes depending on the data provided
 
 
 Advantages:
 - Its loose coupling
 - Easy to debug
-- consistency in Code since every time object is created using Factory rather than using different constructor at different client side.
+- consistency in Code since every time object is created using FactoryMain rather than using different constructor at different client side.
 
 Summary:
-try to use Factory pattern whenever you see an opportunity to encapsulate object creation code and see chance of creating different object in near future.
+try to use FactoryMain pattern whenever you see an opportunity to encapsulate object creation code and see chance of creating different object in near future.
 
  */
 class CurrencyFactory {
--- FactoryDesignPattern/src/main/java/FactoryMain.java
+++ FactoryDesignPattern/src/main/java/FactoryMain.java
@@ -1,5 +1,5 @@
-// Factory client code
-public class Factory {
+// FactoryMain client code
+public class FactoryMain {
     public static void main(String args[]) {
         String country = args[0];
         Currency rupee = CurrencyFactory.createCurrency(country);
